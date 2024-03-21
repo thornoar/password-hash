49c92d8034ace6aa060f94be85a22876008039d2
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-21 20:00:19 +0100

    --

--- phash.hs
+++ phash.hs
@@ -21,13 +21,9 @@ cnk n k
     | k == n = 1
     | k > n = 0
     | otherwise = (cnk (n-1) (k-1)) + (cnk (n-1) k)
--- cnk n k = div (factorial' n k) (factorial k)
 
 dropElementInfo :: ([a], Integer) -> (Integer, Integer)
 dropElementInfo (src, m) = (toInteger $ length src, m)
-
-addLength :: [a] -> ([a], Integer)
-addLength lst = (lst, toInteger $ length lst)
 
 nsInYear :: Double
 nsInYear = 3.15576E16
@@ -36,10 +32,30 @@ nsInYear = 3.15576E16
 class Shifting a where
     shift :: a -> Integer
 
+-- Extending the Shifting typeclass on lists
+instance (Shifting a) => Shifting [a] where
+    shift = sum . (map shift)
+
 -- Characters shift keys by their ACSII values, amplified
 instance Shifting Char where
     shift :: Char -> Integer
     shift c = toInteger $ ord c
+
+mapHashing :: (Shifting c) => Integer -> (Integer -> b -> c) -> (b -> Integer) -> [b] -> [c]
+mapHashing _ _ _ [] = []
+mapHashing key f spr (b:bs) = res : mapHashing nextKey f spr bs
+    where
+    (keyDiv, keyMod) = divMod key $ spr b
+    res = f keyMod b
+    -- keyShift = (sum . map shift) res
+    nextKey = keyDiv + shift res
+
+composeHashing :: (Shifting c) => (Integer -> b -> c) -> (b -> Integer) -> (Integer -> c -> d) -> (Integer -> b -> d)
+composeHashing f spr g key b = g nextKey c
+    where
+    (keyDiv, keyMod) = divMod key $ spr b
+    c = f keyMod b
+    nextKey = keyDiv + shift c
 
 ---------------------------------------------------------
 -- | PRE-DEFINED STRINGS FROM WHICH HASHES WILL BE DRAWN |
@@ -81,23 +97,16 @@ chooseInjectivityRange amt = factorial' (fst amt) (snd amt)
 
 -- Apply the `chooseOrdered` function to a list, modifying the key every time
 mapChooseOrdered :: (Eq a, Shifting a) => Integer -> [([a], Integer)] -> [[a]]
-mapChooseOrdered _ [] = []
-mapChooseOrdered key config = curSelection : mapChooseOrdered nextKey (tail config)
-    where
-    curSrc = head config
-    (keyDiv, keyMod) = divMod key $ chooseInjectivityRange $ dropElementInfo curSrc
-    curSelection = chooseOrdered keyMod curSrc
-    keyShift = (sum . map shift) curSelection
-    nextKey = keyDiv + keyShift
+mapChooseOrdered key = mapHashing key chooseOrdered (chooseInjectivityRange . dropElementInfo)
 
 -- On the integer segment from 0 to [this] the previous function is injective (in fact bijective)
 mapChooseInjectivityRange :: [(Integer, Integer)] -> Integer
 mapChooseInjectivityRange = product . (map chooseInjectivityRange)
 
 -- Mix a list of lists together, keeping the elements of the individual lists in the same order.
-shuffleLists :: (Eq a, Shifting a) => Integer -> [[a]] -> [a]
-shuffleLists _ [] = []
-shuffleLists key srcs = (:) curElt $ shuffleLists nextKey $
+mergeLists :: (Eq a, Shifting a) => Integer -> [[a]] -> [a]
+mergeLists _ [] = []
+mergeLists key srcs = (:) curElt $ mergeLists nextKey $
     (take curIndex srcs)
     ++
     (if (1 < length curLst) then [tail curLst] else [])
@@ -112,21 +121,16 @@ shuffleLists key srcs = (:) curElt $ shuffleLists nextKey $
     nextKey = keyDiv + shift curElt
 
 -- On the integer segment from 0 to [this] the previous function is injective.
-shuffleInjectivityRange :: [Integer] -> Integer
-shuffleInjectivityRange srcs = product $ zipWith (^) [1 .. (toInteger $ length srcs)] (sortBy (flip compare) srcs)
+mergeInjectivityRange :: [Integer] -> Integer
+mergeInjectivityRange srcs = product $ zipWith (^) [1 .. (toInteger $ length srcs)] (sortBy (flip compare) srcs)
 
 -- Get a hash sequence from a key and a source configuration
 getHash :: (Eq a, Shifting a) => Integer -> [([a], Integer)] -> [a]
-getHash key config = shuffleLists nextKey hashSelections
-    where
-    (keyDiv, keyMod) = divMod key $ mapChooseInjectivityRange $ map dropElementInfo config
-    hashSelections = mapChooseOrdered keyMod config
-    keyShift = (sum . map (sum . map shift)) hashSelections
-    nextKey = keyDiv + keyShift
+getHash = composeHashing mapChooseOrdered (mapChooseInjectivityRange . map dropElementInfo) mergeLists
 
 -- All keys between 0 and [this] are guaranteed to give different hashes
 getHashInjectivityRange :: [(Integer, Integer)] -> Integer
-getHashInjectivityRange amts = (mapChooseInjectivityRange amts) * (shuffleInjectivityRange $ map snd amts)
+getHashInjectivityRange amts = (mapChooseInjectivityRange amts) * (mergeInjectivityRange $ map snd amts)
 
 -----------------------------
 -- | MANAGING THE PUBLIC KEY |
@@ -137,9 +141,9 @@ getPublicKey :: String -> Integer
 getPublicKey "" = 0
 getPublicKey (c:cs) = (toInteger $ ord c) * (128 ^ (length cs)) + getPublicKey cs
 
--- Apply public key to shuffle sources
-shuffleSources :: (Eq a, Shifting a) => Integer -> [[a]] -> [[a]]
-shuffleSources pkey srcs = mapChooseOrdered pkey (map addLength srcs)
+-- Apply public key to merge sources
+mergeSources :: (Eq a, Shifting a) => Integer -> [[a]] -> [[a]]
+mergeSources pkey srcs = mapChooseOrdered pkey [(src, toInteger $ length src) | src <- srcs]
 
 ----------------------
 -- | COUNTING NUMBERS |
@@ -151,10 +155,10 @@ numberOfHashes amts = (product $ zipWith cnk fsts snds) * (factorial $ sum snds)
     where
     fsts = map fst amts
     snds = map snd amts
-    
+
 -- Approximately [this] many keys will produce the same hash
 numberOfRepetitions :: [Integer] -> Integer
-numberOfRepetitions = shuffleInjectivityRange
+numberOfRepetitions = mergeInjectivityRange
 
 -- Number of private keys that are guaranteed to produce distinct hashes
 numberOfPrivateKeys :: [(Integer, Integer)] -> Integer
@@ -164,42 +168,66 @@ numberOfPrivateKeys = mapChooseInjectivityRange
 numberOfPublicKeys :: [Integer] -> Integer
 numberOfPublicKeys lens = mapChooseInjectivityRange $ zip lens lens
 
+get128PowerLength :: Integer -> Integer
+get128PowerLength p = 2*p + 3 * pDiv + npMod
+    where
+    (pDiv, pMod) = divMod p 28
+    npMod
+        | pMod < 10 = 1
+        | pMod < 19 = 2
+        | otherwise = 3
+
+getBiggestPower :: Integer -> Integer -> Integer
+getBiggestPower guess bound
+    | (get128PowerLength $ guess + 1) < bound = getBiggestPower (guess + 1) bound
+    | otherwise = guess + 1
+
+maxLengthOfPublicKey :: [Integer] -> Integer
+maxLengthOfPublicKey lens = getBiggestPower 0 $ (toInteger . length . show) (numberOfPublicKeys lens)
+
 --------------------
 -- | USER INTERFACE |
 --------------------
 
 -- Prints help information
 helpAction :: String -> [(Integer, Integer)] -> IO ()
-helpAction cmd amts = case cmd of
-    "#help" -> do
-        putStrLn "usage: phash [#COMMAND [CONFIGURATION] | >COMMAND ARGUMENT | PUBLIC PRIVATE [CONFIGURATION]]"
+helpAction cmd amts
+    | cmd == "--help" || cmd == "-h" = do
+        putStrLn "usage: phash [--OPTION [CONFIGURATION] | PUBLIC PRIVATE [CONFIGURATION]]"
         putStrLn ""
-        putStrLn "commands:"
-        putStrLn "  #help                       show this help message and exit"
-        putStrLn "  #hashes [CONFIGURATION]     print the theoretical number of hashes with given configuration"
-        putStrLn "  #keys [CONFIGURATION]       print the lower range of significant keys in given configuration"
-        putStrLn "  #choices [CONFIGURATION]    print the number of character selections with given configuration"
-        putStrLn "  #shuffles [CONFIGURATION]   print the number of selection shufflings with given configuration"
+        putStrLn "options:"
+        putStrLn "  -h, --help                      show this help message and exit"
+        putStrLn "  -a, --hashes [CONFIGURATION]    print the theoretical number of hashes"
+        putStrLn "  -k, --keys [CONFIGURATION]      print the guaranteed number of keys with distinct hashes"
+        putStrLn "  -t, --times [CONFIGURATION]     calculate the number of years required to crack your passwords"
+        putStrLn ""
+        putStrLn "arguments:"
+        putStrLn "  [CONFIGURATION] stands for a list of string-number pairs that defines the synbols used in hashes"
+        putStrLn "  PUBLIC stands for public key, a memorable string indicative of the password destination"
+        putStrLn "  PRIVATE stands for private key, a large integer known only to the user"
         putStrLn ""
         putStrLn "default configuration:"
         putStrLn $ "  " ++ (show defaultConfiguration)
-    "#hashes" -> do
-        putStrLn $ "total theoretical number of hashes:     " ++ (show $ numberOfHashes amts)
-        putStrLn $ "range of guaranteed hash injectivity:   " ++ (show $ getHashInjectivityRange amts)
-    "#keys" -> do
-        putStrLn $ "number of relevant private keys:       >" ++ (show $ getHashInjectivityRange amts)
-        putStrLn $ "number of keys with different hashes:   " ++ (show $ numberOfPrivateKeys amts)
-        putStrLn $ "number of keys with the same hash:     >" ++ (show $ numberOfRepetitions $ map snd amts)
-    "#times" -> do
-        putStrLn $ "assumed time to check one private key:  " ++ "1 nanosecond = 10^(-9) s"
-        putStrLn $ "time required to brute-force your key:  " ++
+    | cmd == "--hashes" || cmd == "-a" = do
+        putStrLn $ "total theoretical number of hashes:         " ++ (show $ numberOfHashes amts)
+        putStrLn $ "range of guaranteed hash injectivity:       " ++ (show $ getHashInjectivityRange amts)
+    | cmd == "--keys" || cmd == "-k" = do
+        putStrLn $ "number of relevant private keys:           >" ++ (show $ getHashInjectivityRange amts)
+        putStrLn $ "number of keys with different hashes:       " ++ (show $ numberOfPrivateKeys amts)
+        putStrLn $ "number of keys with the same hash:         >" ++ (show $ numberOfRepetitions $ map snd amts)
+        putStrLn $ "maximum relevant length of public keys:     " ++ (show $ maxLengthOfPublicKey $ map fst amts)
+    | cmd == "--times" || cmd == "-t" = do
+        putStrLn $ "assumed time to check one private key:      " ++ "1 nanosecond = 10^(-9) s"
+        putStrLn $ "time required to brute-force your password: " ++
+            (show $ floor $ (fromIntegral $ numberOfHashes amts) / nsInYear) ++ " years"
+        putStrLn $ "time required to brute-force your key:      " ++
             (show $ floor $ (fromIntegral $ numberOfPrivateKeys amts) / nsInYear) ++ " years"
         putStrLn $ "given the final hash, it is impossible to deduce the private key without brute-forcing."
-    _ -> putStrLn "error: help command not recognized"
+    | True = putStrLn "error: help command not recognized"
 
 -- Prints the hash (password) given public and private strings and a hash configuration
 hashAction :: String -> String -> [([Char], Integer)] -> IO ()
-hashAction publicStr privateStr config = putStrLn $ getHash privateKey2 shuffledConfig
+hashAction publicStr privateStr config = putStrLn $ getHash privateKey2 mergedConfig
     where
     publicKey :: Integer
     publicKey = getPublicKey publicStr
@@ -208,17 +236,17 @@ hashAction publicStr privateStr config = putStrLn $ getHash privateKey2 shuffled
     sources = map fst config
     amounts = map snd config
     (privateKey1, privateKey2) = divMod privateBase $ numberOfRepetitions amounts
-    shuffledConfig = zip (shuffleSources publicKey $ shuffleSources privateKey1 sources) amounts
+    mergedConfig = zip (mergeSources publicKey $ mergeSources privateKey1 sources) amounts
 
 -- The main process
 main :: IO ()
 main = do
     args <- getArgs
     case (length args) of
-        0 -> helpAction "#help" []
+        0 -> helpAction "--help" []
         1 -> helpAction (args !! 0) (map dropElementInfo defaultConfiguration)
         2 -> case (args !! 0) of
-            '#':cmd -> helpAction (args !! 0) $ read (args !! 1)
+            '-':cmd -> helpAction (args !! 0) $ read (args !! 1)
             _ -> hashAction (args !! 0) (args !! 1) defaultConfiguration
         3 -> hashAction (args !! 0) (args !! 1) (read $ args !! 2)
         _ -> putStrLn "error: too many arguments"
